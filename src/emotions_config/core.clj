590fbd674f2842b18464b78a70ca401c322e497d
Dave Snowdon <dave@davesnowdon.com>
2014-10-13 08:03:00 +0100

    initial commit with scenebuilder UI

---
+++ src/emotions_config/core.clj
@@ -0,0 +1,29 @@
+(ns emotions-config.core
+  (:require [clojurefx.core :refer :all]
+            [clojure.string :refer [trim]]
+            [clojure.pprint :refer [pprint]]
+            [clj-time.core :as t]
+            [clj-time.format :as tf]
+            [naojure.core :as nao]
+            [emotions.core :refer :all]
+            [emotions.util :refer [float= seconds-diff]])
+  (:import [javafx.fxml FXMLLoader]
+           [javafx.stage StageBuilder]
+           [javafx.scene Scene]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(def stage (run-now
+            (doto
+                (.build (StageBuilder/create))
+              (.setScene (Scene. (FXMLLoader/load (clojure.java.io/resource "editor.fxml")))))))
+
+(defn start []
+  (run-now (.show stage)))
+
+(defn -main
+  [& args]
+  (start)
---
+++ test/emotions_config/core_test.clj
@@ -0,0 +1,7 @@
+(ns emotions-config.core-test
+  (:require [clojure.test :refer :all]
+            [emotions-config.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
